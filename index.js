5934d1c8ec7e2b6398ab8f028264745733b6327f
tryliya <tryliya@gmail.com>
2021-06-06 22:18:47 +0300

    add solutions for tasks

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-const lodash = require('lodash');
+const _ = require('lodash');
+
+
+const data1 = [
+    {
+    "age": 3,
+    "name": "Curtis Greene",
+    "email": "curtis.greene@zaj.ca",
+    },
+    {
+    "age": 23,
+    "name": "Nikki Lowe",
+    "email": "nikki.lowe@darwinium.tv",
+    },
+    {
+    "age": 39,
+    "name": "Barr Copeland",
+    "email": "barr.copeland@nipaz.me",
+    },
+    ];
+    const data2 = [
+    {
+    "age": 29,
+    "name": "Calhoun Woodward",
+    "email": "calhoun.woodward@medmex.info",
+    },
+    {
+    "age": 21,
+    "name": "Leta Lee",
+    "email": "leta.lee@qnekt.com",
+    },
+    {
+    "age": 40,
+    "name": "James Dinh",
+    "email": "j.dink@erw.com",
+    }
+    ];
+
+
+//1
+    let userIndex = _.findIndex(data1, item => item.age === 23);
+
+//2
+    let data3 = _.concat(data1, data2);
+
+//3
+    let object = {
+        "age": 40,
+        "name": "James Dinh",
+        "email": "j.dink@erw.com",
+        };
+
+    let newUser = _.omit(object, ["age"])
+//4
+   
+    let omit = (object) => {let {age, ...newObject} = object; return newObject}      
+    let newUser2 = omit(object)
